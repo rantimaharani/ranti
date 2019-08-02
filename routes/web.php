d7f0103325d1e738b53be7a4b78cbf341fa8ddde
Ranti Maharani <rantimaharani34@gmail.com>
2019-08-02 11:18:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home','HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'],
+function (){
+    Route::get('/',function(){
+        return view('home');
+    });
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
